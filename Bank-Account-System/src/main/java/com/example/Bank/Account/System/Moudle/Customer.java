866940c884d5c11da8ce2fedbee362857f91cd73
Essra <esraaAbdullah94@gmail.com>
2023-05-09 11:00:36 +0400

    Customer Module

--- Bank-Account-System/src/main/java/com/example/Bank/Account/System/Moudle/Customer.java
+++ Bank-Account-System/src/main/java/com/example/Bank/Account/System/Moudle/Customer.java
@@ -5,17 +5,17 @@ import lombok.Getter;
 import lombok.Setter;
 
 import javax.persistence.*;
+
 @Getter
 @Setter
-@Entity
 @Data
-public class Customer extends BaseEntity{
+@Entity
+public class Customer extends BaseEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     Integer id;
     String name;
     String email;
     String phone;
-
 
 }
