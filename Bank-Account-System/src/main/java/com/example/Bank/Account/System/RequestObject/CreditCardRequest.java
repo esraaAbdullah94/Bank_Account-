fa8ada165efbc78eb0d412a2081629317b5e6d4a
Essra <esraaAbdullah94@gmail.com>
2023-05-15 10:20:56 +0400

    starting DTO

---
+++ Bank-Account-System/src/main/java/com/example/Bank/Account/System/RequestObject/CreditCardRequest.java
@@ -0,0 +1,11 @@
+package com.example.Bank.Account.System.RequestObject;
+
+public class CreditCardRequest {
+    private Integer cardNumber;
+
+    private Double creditLimit;
+    private  String createdDate;
+    private Boolean isActive;
+    private Integer customerId;
+
+}
